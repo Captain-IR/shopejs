93fc0d38cbdf27a90a113021ce500b91dab7564a
Captain IR <52345388+Captain-IR@users.noreply.github.com>
2020-08-31 13:37:19 +0200

    Refactoring

--- models/product.js
+++ models/product.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
+	path.dirname(process.mainModule.filename),
+	'data',
+	'products.json'
 );
 
 const getProductsFromFile = (cb) => {
@@ -18,23 +20,45 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-	constructor(title, imageUrl, price, description) {
-		this.title = title
+	constructor(id, title, imageUrl, description, price) {
+		this.id = id;
+		this.title = title;
 		this.imageUrl = imageUrl;
-		this.price = price;
 		this.description = description;
+		this.price = price;
 	}
 
 	save() {
 		getProductsFromFile((products) => {
-			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), (err) => {
-				console.log(err);
-			});
+			if (this.id) {
+				const existingProductIndex = products.findIndex(
+					(prod) => prod.id === this.id
+				);
+				const updatedProducts = [...products];
+				updatedProducts[existingProductIndex] = this;
+				fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+					console.log(err);
+				});
+			} else {
+				this.id = Math.random().toString();
+				products.push(this);
+				fs.writeFile(p, JSON.stringify(products), (err) => {
+					console.log(err);
+				});
+			}
 		});
 	}
 
+	static deleteById
+
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
+	}
+
+	static findById(id, cb) {
+		getProductsFromFile((products) => {
+			const product = products.find((p) => p.id === id);
+			cb(product);
+		});
 	}
 };
